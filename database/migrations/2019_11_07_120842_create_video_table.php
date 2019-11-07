158220b54ab69d707486057ef4fa6442f6fb2b70
gael.christe <gael.christe@he-arc.ch>
2019-11-07 13:41:06 +0100

    migration

---
+++ database/migrations/2019_11_07_120842_create_video_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name')->nullable(false);
+            $table->binary('video')->nullable(false);
+            $table->time('duration')->nullable(false);
+            $table->boolean('public')->nullable(false);
+            $table->integer('fk_owner')->nullable(false);
+            $table->timestamp('video_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="title m-b-md">PoorTube</div>
+
+                
+        </div>
+    </div>
+</div>
+
+@endsection
