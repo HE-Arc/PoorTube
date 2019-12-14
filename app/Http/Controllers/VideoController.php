5f1dc7c5ce1435c08a99c890b6ad8891046c0a20
gael.christe <gael.christe@he-arc.ch>
2019-12-14 10:30:16 +0100

    cards display, font style, fix problem with delete button

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -13,154 +13,135 @@ use function PHPSTORM_META\type;
 
 class VideoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //$videos = Video::all();//Video::latest()->paginate(5);
-        $videos = DB::select('SELECT * from video order by created_at DESC');
-        // Pour la version pagination ajouter : {!! $videos->links() !!} dans index.blade.php après END VIDEOS
-        $likes = Like::all();
-        return view('videos.index', compact(['videos', 'likes']));//->with('i', (request()->input('page', 1)-1)*5);
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    //$videos = Video::all();//Video::latest()->paginate(5);
+    $videos = DB::select('SELECT * from video order by created_at DESC');
+    // Pour la version pagination ajouter : {!! $videos->links() !!} dans index.blade.php après END VIDEOS
+    $likes = Like::all();
+    return view('videos.index', compact(['videos', 'likes'])); //->with('i', (request()->input('page', 1)-1)*5);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('videos.create');
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('videos.create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required',
-            'video' => 'required',
-        ]);
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $request->validate([
+      'name' => 'required',
+      'video' => 'required',
+    ]);
 
-        $input['video'] = time() . '.' . $request->video->getClientOriginalExtension();
-        $request->video->move(public_path('videos-gallery'), $input['video']);
+    $input['video'] = time() . '.' . $request->video->getClientOriginalExtension();
+    $request->video->move(public_path('videos-gallery'), $input['video']);
 
-        if(Auth::check())
-        {
-          $input['name'] = $request->name;
-          $input['public'] = 1 ; //$request->input("public");
-          $input['fk_owner'] = Auth::id();
-          $input['author'] = Auth::user()->name;
+    if (Auth::check()) {
+      $input['name'] = $request->name;
+      $input['public'] = 1; //$request->input("public");
+      $input['fk_owner'] = Auth::id();
+      $input['author'] = Auth::user()->name;
 
-          Video::create($input);
+      Video::create($input);
 
-          return redirect()->route('videos.index')->with('success', 'Video created successfully');
-        }
-        
-        return redirect()->route('videos.index')->with('warning', 'You must be connected');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Video  $video
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Video $video)
-    {
       return redirect()->route('videos.index')->with('success', 'Video created successfully');
-        //return view('videos.show', compact($video));
     }
 
-    public function allVideos()
-    {
-      // $myVideos = DB::table('video')->where('fk_owner', (string)Auth::id());
-      // die((string)$myVideos);
-      if(Auth::check())
-      {
-        $myVideos = DB::select('select * from video where fk_owner = ?', [Auth::id()]);
-        $likes = Like::all();
-        return view('videos.myvideo', compact(['myVideos', 'likes']));//->with('i', (request()->input('page', 1)-1)*5);
+    return redirect()->route('videos.index')->with('warning', 'You must be connected');
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Video  $video
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Video $video)
+  {
+    return redirect()->route('videos.index')->with('success', 'Video created successfully');
+    //return view('videos.show', compact($video));
+  }
+
+  public function allVideos()
+  {
+    // $myVideos = DB::table('video')->where('fk_owner', (string)Auth::id());
+    // die((string)$myVideos);
+    if (Auth::check()) {
+      $videos = DB::select('select * from video where fk_owner = ?', [Auth::id()]);
+      $likes = Like::all();
+      return view('videos.myvideo', compact(['videos', 'likes'])); //->with('i', (request()->input('page', 1)-1)*5);
+    }
+
+    return redirect()->route('videos.index')->with('warning', 'you must be connected');
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Video  $video
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Video $video)
+  {
+    //return view('videos.edit', compact($video));
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Video  $video
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Video $video)
+  { }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Video  $video
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Video $video)
+  {
+    $video->delete();
+    $videos = DB::select('select * from video where fk_owner = ?', [Auth::id()]);
+    $likes = Like::all();
+    return view('videos.myvideo', compact(['videos', 'likes']))->with('success', 'Video deleted successfully');
+  }
+
+  public function like($video_id)
+  {
+    if (Auth::check()) {
+      $input['video_id'] = $video_id;
+      $input['user_id'] = Auth::id();
+      if (Like::where('user_id', '=', $input['user_id'])->where('video_id', '=', $input['video_id'])->exists()) {
+        Like::where('user_id', '=', $input['user_id'])->where('video_id', '=', $input['video_id'])->delete();
+        return back()->with('success', 'disliked successfully');
+      } else {
+        Like::create($input);
+        return back()->with('success', 'liked successfully');
       }
-
-      return redirect()->route('videos.index')->with('warning', 'you must be connected');      
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Video  $video
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Video $video)
-    {
-        //return view('videos.edit', compact($video));
-    }
+    return back()->with('warning', 'you must be connected'); //redirect()->route('videos.index')->with('success', 'you must be connected');
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Video  $video
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Video $video)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Video  $video
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Video $video)
-    {
-        $video->delete();
-        $myVideos = DB::select('select * from video where fk_owner = ?', [Auth::id()]);
-        $likes = Like::all();
-        return view('videos.myvideo', compact(['myVideos', 'likes']))->with('success', 'Video deleted successfully');
-    }
-
-    public function like($video_id) {
-      if(Auth::check())
-      {
-        $input['video_id'] = $video_id;
-        $input['user_id'] = Auth::id();
-        if(Like::where('user_id', '=', $input['user_id'])->where('video_id', '=', $input['video_id'])->exists()) {
-          Like::where('user_id', '=', $input['user_id'])->where('video_id', '=', $input['video_id'])->delete();
-          return back()->with('success', 'disliked successfully');
-        } else {
-          Like::create($input);
-          return back()->with('success', 'liked successfully');
-        }
-      }
-
-      return back()->with('warning', 'you must be connected');//redirect()->route('videos.index')->with('success', 'you must be connected');
-      
-    }
-
-    // public static function doYouLike($video_id) {
-    //   // TODO look if id is checked
-    //   if(Auth::check())
-    //   {
-    //     $input['video_id'] = $video_id;
-    //     //$input['user_id'] = Auth::id();
-    //     $input['user_id'] = Auth::id();
-    //     if(Like::where('user_id', '=', $input['user_id'])->where('video_id', '=', $input['video_id'])->exists()) {
-    //       return 1;
-    //     } else {
-    //       return 0;
-    //     }
-    //   }
-    //   return 0;
-    // }
+  }
 }
--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -1,158 +1,76 @@
-@extends('videos.layout')
+<!DOCTYPE html>
+<html>
 
-@section('content')
+<head>
+    <title>PoorTube</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<div class="column is-8 is-offset-2">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+</head>
 
-    <!-- <div class="pull-right">
-        <a class="button is-link is-light" href="{{ route('videos.index') }}"> Back</a>
-    </div> -->
+<body>
 
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
+    <div class="container">
+        <div class="column is-8 is-offset-2">
 
-    <form action="{{ route('videos.store') }}" method="POST" enctype="multipart/form-data">
-        {!! csrf_field() !!}
-
-        <div class="field">
-            <label class="label">Name</label>
-            <div class="control">
-                <input class="input" type="text" placeholder="Text input" name="name">
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
-        </div>
+            @endif
 
-
-        <div class="field">
-            <div class="control">
-                <strong>Video:</strong>
-                <div class="file">
-                    <label class="file-label">
-                        <input class="file-input" type="file" name="video">
-                        <span class="file-cta">
-                            <span class="file-icon">
-                                <i class="fas fa-upload"></i>
-                            </span>
-                            <span class="file-label">
-                                Choose a file…
-                            </span>
-                        </span>
-                    </label>
+            <form action="{{ route('videos.store') }}" method="POST" enctype="multipart/form-data">
+                {!! csrf_field() !!}
+                <div class="field">
+                    <label class="label">Name</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Text input" name="name">
+                    </div>
                 </div>
-
-            </div>
-        </div>
-
-       
-
-        <div class="field">
-            <div class="control">
-                <label class="checkbox">
-                    <input type="checkbox" name="public">
-                    Public
-                </label>
-            </div>
-        </div>
-
-
-
-        <div class="field is-grouped">
-            <div class="control">
-                <button type="submit" class="button is-link">Submit</button>
-            </div>
-            <div class="control">
-                <a class="button is-link is-light" href="{{ url()->previous() }}">Cancel</a>
-            </div>
-        </div>
-
-        
-
-    </form>
-</div>
-
-<!-- <div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Add New Video</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('videos.index') }}"> Back</a>
+                <div class="field">
+                    <div class="control">
+                        <strong>Video:</strong>
+                        <div class="file">
+                            <label class="file-label">
+                                <input class="file-input" type="file" name="video">
+                                <span class="file-cta">
+                                    <span class="file-icon">
+                                        <i class="fas fa-upload"></i>
+                                    </span>
+                                    <span class="file-label">
+                                        Choose a file…
+                                    </span>
+                                </span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <label class="checkbox">
+                            <input type="checkbox" name="public">
+                            Public
+                        </label>
+                    </div>
+                </div>
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button type="submit" class="button is-link">Submit</button>
+                    </div>
+                    <div class="control">
+                        <a class="button is-link is-light" href="{{ url()->previous() }}">Cancel</a>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-</div>
-   
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-   
-<form action="{{ route('videos.store') }}" method="POST" enctype="multipart/form-data">
-{!! csrf_field() !!}
-  
-     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="name" class="form-control" placeholder="Name">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Video:</strong>
-                <input type="file" name="video" class="form-control">
-            </div>
-        </div> -->
-<!-- <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Public:</strong>
-                <input type="text" name="public">
-            </div>
-        </div> -->
-<!-- <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </div> -->
 
-<!-- <div class="row">
+</body>
 
-                <div class="col-md-5">
-
-                    <strong>Title:</strong>
-
-                    <input type="text" name="name" class="form-control" placeholder="Title">
-
-                </div>
-
-                <div class="col-md-5">
-
-                    <strong>Video:</strong>
-
-                    <input type="file" name="video" class="form-control">
-
-                </div>
-
-                <div class="col-md-2">
-
-                    <br />
-
-                    <button type="submit" class="btn btn-success">Upload</button>
-
-                </div>
-
-            </div> -->
-
-<!-- </form> -->
-@endsection
+</html>
