594abf78f438753245455eca18af5a4331472876
colin.friche <colin.friche@he-arc.ch>
2019-12-16 13:33:03 +0100

    changed to be https friendly + added verification on empty comments

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -23,7 +23,7 @@ class VideoController extends Controller
   {
     $videos = DB::select('SELECT * from video order by created_at DESC');
     $likes = Like::all();
-    return view('videos.index', compact(['videos', 'likes'])); 
+    return view('videos.index', compact(['videos', 'likes']));
   }
 
   /**
@@ -60,10 +60,10 @@ class VideoController extends Controller
 
       Video::create($input);
 
-      return redirect()->route('videos.index')->with('success', 'Video created successfully');
+      return redirect()->route('videos.index')->with('success', 'Video created successfully.');
     }
 
-    return redirect()->route('videos.index')->with('warning', 'You must be connected');
+    return redirect()->route('videos.index')->with('warning', 'You must be connected.');
   }
 
   /**
@@ -74,7 +74,7 @@ class VideoController extends Controller
    */
   public function show(Video $video)
   {
-    return redirect()->route('videos.index')->with('success', 'Video created successfully');
+    return redirect()->route('videos.index')->with('success', 'Video created successfully.');
     //return view('videos.show', compact($video));
   }
 
@@ -88,7 +88,7 @@ class VideoController extends Controller
       return view('videos.myvideo', compact(['videos', 'likes'])); //->with('i', (request()->input('page', 1)-1)*5);
     }
 
-    return redirect()->route('videos.index')->with('warning', 'you must be connected');
+    return redirect()->route('videos.index')->with('warning', 'You must be connected.');
   }
 
   /**
@@ -133,14 +133,14 @@ class VideoController extends Controller
       $input['user_id'] = Auth::id();
       if (Like::where('user_id', '=', $input['user_id'])->where('video_id', '=', $input['video_id'])->exists()) {
         Like::where('user_id', '=', $input['user_id'])->where('video_id', '=', $input['video_id'])->delete();
-        return back()->with('success', 'disliked successfully');
+        return back()->with('success', 'Disliked successfully.');
       } else {
         Like::create($input);
-        return back()->with('success', 'liked successfully');
+        return back()->with('success', 'Liked successfully.');
       }
     }
 
-    return back()->with('warning', 'you must be connected'); //redirect()->route('videos.index')->with('success', 'you must be connected');
+    return back()->with('warning', 'You must be connected.'); //redirect()->route('videos.index')->with('success', 'you must be connected');
 
   }
 
@@ -148,24 +148,28 @@ class VideoController extends Controller
   {
     if(Auth::check())
     {
-      $input['video_id'] = $request->video_id;
-      $input['user_id'] = Auth::id();
-      $input['user_name'] = Auth::user()->name;
-      $input['comment'] = $request->comment;
+        if($request->comment != '')
+        {
+            $input['video_id'] = $request->video_id;
+            $input['user_id'] = Auth::id();
+            $input['user_name'] = Auth::user()->name;
+            $input['comment'] = $request->comment;
 
-      Comment::create($input);
+            Comment::create($input);
 
-      return back()->with('success', 'commented successfully');
+            return back()->with('success', 'Comment successfully published.');
+        }
+        return back()->with('warning', "You can't publish an empty comment.");
     }
 
-    return back()->with('warning', 'you must be connected');
+    return back()->with('warning', 'You must be connected.');
   }
 
   public function deleteComment($comment_id)
   {
     $comment = Comment::find($comment_id);
     $comment->delete();
-    
+
     return back()->with('success', 'Comment deleted successfully');
   }
 }
--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -7,8 +7,8 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
-    <script src="http://malsup.github.com/jquery.form.js"></script>
+    <script src="/js/ajax.jquery.js"></script>
+    <script src="/js/jquery.form.js"></script>
 </head>
 
 <body>
